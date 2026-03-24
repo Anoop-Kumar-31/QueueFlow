cf85b6ab56af1c63162d44d5aecbaf42202423d1
Anoop Kumar <amt312002@gmail.com>
2026-03-24 16:57:36 +0530

    feat: added dynamic backend endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = import.meta.env.VITE_API_URL.endsWith("/") ? import.meta.env.VITE_API_URL.slice(0, -1) + '/api' : import.meta.env.VITE_API_URL + "/api";
 
 export const loginAPI = async (email, password) => {
   const res = await fetch(`${BASE_URL}/auth/login`, {
